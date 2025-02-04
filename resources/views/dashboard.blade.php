fdb370132130b33951f71e68b5d72298b7e03ce6
Jorge <jorge.troncoso4@gmail.com>
2025-02-04 10:37:45 -0300

    NOTASDEVERSION

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
+            {{ __('Inicio') }}
         </h2>
     </x-slot>
 
@@ -10,8 +10,13 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
                     {{ __("haz ingresado al sistema!") }}
-                    <h2>Proximamente nuevas funcionalidades 😉</h2>
-
+                    <h1>Proximamente nuevas funcionalidades 😉</h1>
+                    <hr><br>
+                    <h2>Notas de la version:</h2>
+                    <li>Apartado de Administracion de Oficio y Libertades deben actualizar dos veces la pestaña para un correcto funcionamiento</li>
+                    <li>Seccion de buscar no realiza la busqueda por Numero entregado, pero si por los demas apartados</li>
+                    <li>En el primer ingreso deben realizar el cambio de contraseña esto se realiza en la pestaña superior derecha al precionar su nombre posteriormente en Perfil</li>
+                    <li>Cualquier sugerencia u error encontrado favor avisar al correo jtroncosor@pjud.cl</li>
                 </div>
             </div>
         </div>
--- resources/views/livewire/layout/navigation.blade.php
+++ resources/views/livewire/layout/navigation.blade.php
@@ -31,7 +31,7 @@ new class extends Component
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" wire:navigate>
-                        {{ __('Dashboard') }}
+                        {{ __('Inicio') }}
                     </x-nav-link>
                 </div>
                 <!--botton en navbar-->
@@ -100,7 +100,7 @@ new class extends Component
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" wire:navigate>
-                {{ __('Dashboard') }}
+                {{ __('Inicio') }}
             </x-responsive-nav-link>
         </div>
     
@@ -134,13 +134,13 @@ new class extends Component
 
             <div class="mt-3 space-y-1">
                 <x-responsive-nav-link :href="route('profile')" wire:navigate>
-                    {{ __('Profile') }}
+                    {{ __('Perfil') }}
                 </x-responsive-nav-link>
 
                 <!-- Authentication -->
                 <button wire:click="logout" class="w-full text-start">
                     <x-responsive-nav-link>
-                        {{ __('Log Out') }}
+                        {{ __('Salir') }}
                     </x-responsive-nav-link>
                 </button>
             </div>
