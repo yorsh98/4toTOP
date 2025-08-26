cdea583a416ac77d162da389057d4d76fe78a90b
yorsh98 <jorge.troncoso4@gmail.com>
2025-08-26 14:57:57 -0400

    ActualizacionDependencias

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import livewire from '@defstudio/vite-livewire-plugin';
+
 
 export default defineConfig({
     plugins: [
@@ -7,5 +9,6 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
+        livewire(),
     ],
 });
